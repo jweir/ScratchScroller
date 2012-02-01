8f2e982cb83bfdea74a44d2301a432be26879441
J. Weir <john@smokinggun.com>
2012-02-01 13:25:00 -0500

    IOS is in a loop

--- js/scroll-to.js
+++ js/scroll-to.js
@@ -3,21 +3,37 @@
 
   var timer;
 
+  var v = 0;
+
+  function compensate(){
+    // Compensate for the scoll getting out of alignment
+    var x = parseInt($(this).css("top"),10),
+    y = $(window).scrollTop()
+    self = this;
+
+    $("#console").text(v++);
+    sn.halt(function(){
+      $(self).css({top:0});
+      $(window).scrollTop(y -x);
+    })
+  }
+
   function scroll(el, top){
     // TODO do not animate if the element is already in place
-    // if(Math.floor(top) == el.css(top);
+    // if(Math.floor(top) == parseInt(el.css(top),10)){
+    // alert("k");
+    // return true;
+    // }
 
-    // TODO detect if CSS transitions are supported, if not
-    // animate via jquery
     el.stop().animate({
-      top      : top,
-      easing   : "easeInQuint",
-      duration : 500
-    });
+      top      : top},
+      {
+        easing   : "easeOutExpo",
+        duration : 500,
+        complete : compensate
+      });
 
-    // el.css({top: top})
-
-    return el;
+      return el;
   }
 
   $.fn.scrollTo = function(top, options){
@@ -33,15 +49,16 @@
 (function(){ //ScrollLock
 
   var timer,
-      selector;
+  selector;
 
   function init($selector){
     selector   = $selector;
     $(window).resize(deferResize);
-    return set($(collection()[0]));
+    return set(select($(collection()[0])));
   }
 
   function refresh(){
+    console.log("refresh");
     return set(find());
   }
 
@@ -59,6 +76,7 @@
   }
 
   function set(o){
+    console.log("set");
     return scroll(o[0] ? o : find());
   }
 
@@ -75,18 +93,18 @@
 
   function offBy(el){
     var elTop = $(el).offset().top - $(el).parent().offset().top,
-        elH   = $(el).outerHeight(),
-        top   = $(window).scrollTop();
-        cen   = top + $(window).height()/2;
+    elH   = $(el).outerHeight(),
+    top   = $(window).scrollTop();
+    cen   = top + $(window).height()/2;
 
-    return cen - elTop - (elH/2);
+    return parseInt(cen - elTop - (elH/2), 10);
   }
 
   function inCenter(el){
     var elTop = $(el).offset().top,
-        elH   = $(el).outerHeight(),
-        top   = $(window).scrollTop();
-        cen   = top + $(window).height()/2;
+    elH   = $(el).outerHeight(),
+    top   = $(window).scrollTop();
+    cen   = top + $(window).height()/2;
 
     return elTop <= cen && (elTop + elH) > cen;
   }
@@ -101,6 +119,14 @@
     timer = setTimeout(refresh, 25);
   }
 
+  function halt(fn){
+    $("body").addClass("halt");
+    sn.toggle(false);
+    fn();
+    setTimeout(function(){sn.toggle(true); $("body").removeClass("halt");},1000);
+
+  }
+
   window.sn = {
     init       : init,
     collection : collection,
@@ -109,19 +135,34 @@
     next       : next,
     find       : find,
     refresh    : refresh,
-    offset     : offBy
+    offset     : offBy,
+    halt       : halt
   };
 
 }());
 
 (function($){ // MouseWheel events
 
-  var timer;
+  var timer,
+      enabled = true;
+
+  window.sn.toggle = function(state){
+    enabled = state;
+    return enabled;
+  }
+
   $(document).on("scroll",function(e){
-    sn.find();
-    clearTimeout(timer);
-    timer = setTimeout(function(){ sn.set(sn.find()) }, 200);
-    return true;
+    if(enabled){
+      sn.find();
+      clearTimeout(timer);
+      timer = setTimeout(function(){
+        console.log("scroll call");
+        sn.set(sn.find()) }, 200);
+      return true;
+    } else {
+      e.preventDefault();
+      return false;
+    }
   })
 
 }(jQuery));
